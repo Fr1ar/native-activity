b34f395033e776958414cc25add5049022e8bb80
Konstantin Stankevich <fr1ar@mail.ru>
2023-02-20 10:37:46 +0300

    Transparent

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -169,8 +169,8 @@ static void engine_draw_frame(struct engine* engine) {
   }
 
   // Just fill the screen with a color.
-  glClearColor(((float)engine->state.x) / engine->width, engine->state.angle,
-               ((float)engine->state.y) / engine->height, 1);
+  glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
+
   glClear(GL_COLOR_BUFFER_BIT);
 
   eglSwapBuffers(engine->display, engine->surface);
--- app/src/main/java/com/dynamo/android/DefoldActivity.java
+++ app/src/main/java/com/dynamo/android/DefoldActivity.java
@@ -47,6 +47,10 @@ import java.util.ArrayList;
 
 import android.content.pm.PackageInfo;
 
+
+// https://github.com/defold/defold/blob/master/engine/glfw/java/com/dynamo/android/DefoldActivity.java
+
+// https://github.com/defold/defold/blob/master/engine/glfw/lib/android/android_window.c
 public class DefoldActivity extends NativeActivity {
 
     // Must match values from sys.h
